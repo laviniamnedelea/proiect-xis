811d2c1cf59285c2c43d116210746259563f8f00
Mircea Hanghiuc <mirceah99@gmail.com>
2022-05-09 12:10:18 +0300

    add parse json functionality

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
 const textArea = document.getElementById("textArea");
-
-function showContent(filename) {
-	fetch(`https://mirceah99.github.io/proiect-xis/${filename}`)
-		.then((response) => response.body)
-		.then((body) => {
-			const reader = body.getReader();
-			reader.read().then((r) => {
-				let string = new TextDecoder().decode(r.value);
-				string = string.replaceAll("<", "&lt;");
-				string = string.replaceAll(">", "&gt;");
-				textArea.classList.remove("hide");
-				console.log(string);
-				textArea.innerHTML = string;
-			});
-		});
+const parsed = document.getElementById("parsed");
+async function showContent(filename) {
+	getTextFromGit(filename).then((text) => {
+		let string;
+		string = text.replaceAll("<", "&lt;");
+		string = string.replaceAll(">", "&gt;");
+		textArea.classList.remove("hide");
+		textArea.innerHTML = string;
+	});
 }
 
 function readTextFile(file) {
@@ -28,4 +22,84 @@ function readTextFile(file) {
 		}
 	};
 	rawFile.send(null);
+}
+
+async function parseJson() {
+	let data = await getTextFromGit(
+		"EvidentaAgentieFotomodeleHanghiucMircea.json"
+	);
+	data = JSON.parse(data);
+	parsed.innerHTML = `
+    <h2>Fotomodele agentiei ${data.agentie.nume} cui(${data.agentie.CUI}) infintata la(${data.agentie["data-infintare"]}):</h2>
+			<table id="models"></table>
+    <h2>Evenimentele:</h2>
+    <table id="events"></table>`;
+
+	// add models
+	let table = document.getElementById("models");
+	table.innerHTML = "";
+	addRowToTable(table, [
+		"Nume",
+		"CNP",
+		"Inaltime",
+		"Greutate",
+		"Culoare par",
+		"Sex",
+		"Telefon",
+		"Data nasterii",
+	]);
+	data.agentie.fotomodele.forEach((element) => {
+		addRowToTable(table, [
+			element.nume,
+			element.cnp,
+			element.inaltime,
+			element.greutate,
+			element.culoarePar,
+			element.sex,
+			element.telefon,
+			element["data-nasterii"],
+		]);
+	});
+
+	// add events
+	table = document.getElementById("events");
+	addRowToTable(table, [
+		"Denumire",
+		"Data",
+		"Durata (ore)",
+		"Descriere",
+		"Fotomodele",
+	]);
+	data.agentie.evenimente.forEach((eveniment) => {
+		const models = eveniment.fotomodele.reduce((prevValue, currValue) => {
+			prevValue = prevValue + ` ${currValue.nume},`;
+			return prevValue;
+		}, "");
+
+		addRowToTable(table, [
+			eveniment.detalii.denumire,
+			eveniment.detalii.data,
+			eveniment.detalii.durata,
+			eveniment.detalii.descriere,
+			models.substring(0, models.length - 1),
+		]);
+	});
+}
+function getTextFromGit(filename) {
+	return fetch(`https://mirceah99.github.io/proiect-xis/${filename}`)
+		.then((response) => response.body)
+		.then((body) => {
+			const reader = body.getReader();
+			return reader.read().then((r) => {
+				let string = new TextDecoder().decode(r.value);
+				return string;
+			});
+		});
+}
+
+function addRowToTable(table, data) {
+	const row = table.insertRow();
+	data.forEach((element, index) => {
+		row.insertCell(index).innerHTML = element;
+	});
 }
